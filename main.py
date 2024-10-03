9577b8ffa8623d6210f702b40da74b19ab0d4a75
SofiaHartova <147645834+SofiaHartova@users.noreply.github.com>
2024-10-03 16:29:20 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Login page
+@app.route('/login', methods=['GET'])
+def login_page():
+    return render_template("login_page.html")
+
+# Processing login and password
+@app.route('/login', methods=['POST'])
+def login():
+    pass
+
+# Action selection page (fill in data/view data)
+@app.route('/action', methods=['GET'])
+def action_page():
+    pass
+
+# First block of questions
+@app.route('/questions1', methods=['POST'])
+def questions1():
+    pass
+
+# Second block of questions
+@app.route('/questions2', methods=['POST'])
+def questions2():
+    pass
+
+# Third block of questions
+@app.route('/questions3', methods=['POST'])
+def questions3():
+    pass
+
+# School data page
+@app.route('/school-data', methods=['GET'])
+def school_data():
+    pass
+
+# School comparison page
+@app.route('/compare-schools', methods=['GET'])
+def compare_schools():
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
