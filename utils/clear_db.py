71c47be5e9a40d457a4cb5e34094a462256efebd
Арсений Пирогов <pirogovarsenij403@gmail.com>
2025-01-07 20:10:19 +0500

    feat: fill events table

--- utils/clear_db.py
+++ utils/clear_db.py
@@ -1,9 +1,13 @@
 from db.db import db
-from db.models import EducationalOrganization, Event
+from db.models import EducationalOrganization, Event, EducationalOrganizationEvent
 from main import app
 
 
 with app.app_context():
+    db.session.query(
+        EducationalOrganizationEvent
+    ).delete() 
+    db.session.commit()
     db.session.query(
         EducationalOrganization
     ).delete()  # Очистка таблицы EducationalOrganization
